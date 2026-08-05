d354c09d2690c3811027fcce2ed69b83aa71f4da
Matthew Pham <matthewpham03@gmail.com>
2026-08-05 16:26:48 -0700

    Add project content data

---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        id: "soccer-predictor",
+        title: "National Mens Team Predictor",
+        tag: "Machine Learning",
+        description: "Implemented ...",
+        stack: ["Python", "NumPy", "Scikit-learn"],
+        link: "https://github.com/matthewphamm/national-mens-team-predictor"
+    }
+];
+
+export const skills = [
+    "Python", "Git", "Linux",
+];
