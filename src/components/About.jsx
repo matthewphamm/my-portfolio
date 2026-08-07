e2c0a16c8a323944b101ba700263381f6ff0d080
Matthew Pham <matthewpham03@gmail.com>
2026-08-06 17:09:27 -0700

    Edit about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,14 @@ export default function About() {
         <section id="about" className="max-w-4xl mx-auto px-6 py-20 border-t border-line">
             <p className="font-mono text-sm text-cobalt mb-4">// 01 - about</p>
             <p className="font-body text-m leading-relaxed max-w-2xl">
-                I am a Computer Science student concentrating in Artificle Intelligence. 
+                I am a Computer Science student concentrating in Artificial Intelligence. I am consistently growing and learning, with hands-on experience
+                in building multiple GitHub projects, including data science applications, machine learning models, and AI-powered applications. Currently, 
+                I am learning how AI agents can help my workflow and using AI api's for applications. 
+                <br />
+                <br />
+                In my free time, I enjoy outdoor activities such as hiking, soccer, and running. I'm also an avid film enthusiast, drawn to the creativity, 
+                cinematography, and storytelling that great films offer. I am currently seeking an internship opportunity to strengthen my skills and
+                gain hands-on, real-world experience.
             </p>
         </section>
     );
